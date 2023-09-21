21e3c7211838af0e5c6c6851f3998fce1ed77d03
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-09-21 03:48:48 +0200

    Create `@ControllerHandler` annotation for all controllers.

--- src/main/java/com/example/customcontrolleradvicewithaop/CustomControllerAdviceWithAopApplication.java
+++ src/main/java/com/example/customcontrolleradvicewithaop/CustomControllerAdviceWithAopApplication.java
@@ -2,8 +2,10 @@ package com.example.customcontrolleradvicewithaop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class CustomControllerAdviceWithAopApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/customcontrolleradvicewithaop/controllers/SimpleController.java
+++ src/main/java/com/example/customcontrolleradvicewithaop/controllers/SimpleController.java
@@ -1,9 +1,11 @@
 package com.example.customcontrolleradvicewithaop.controllers;
 
+import com.example.customcontrolleradvicewithaop.annotations.ControllerHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
+@ControllerHandler
 public class SimpleController {
 }
